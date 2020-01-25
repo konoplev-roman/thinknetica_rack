315486a30c6d8a32a9785e7927dd2dc2c5a43d81
konoplev-roman <roman@konoplev.pro>
2020-01-25 20:01:00 +0300

    Add request processing on GET /time

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 class App
-  def call(_env)
-    build_response("Hello, World!\n")
+  def call(env)
+    request = Rack::Request.new(env)
+
+    perform_request(request)
   end
 
   private
+
+  def perform_request(request)
+    return not_found unless request.get? && request.path == '/time'
+
+    build_response("Hello, World!\n")
+  end
+
+  def not_found
+    build_response("Not found!\n", status: 404)
+  end
 
   def build_response(body, status: 200)
     [status, { 'Content-Type' => 'text/plain' }, [body]]
