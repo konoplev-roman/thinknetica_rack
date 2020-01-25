63c281edf260b3f20a35cb6f8fea22fb5fc241cf
konoplev-roman <roman@konoplev.pro>
2020-01-25 18:09:31 +0300

    Add a simple rack-interface

---
+++ app.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class App
+  def call(_env)
+    [status, headers, body]
+  end
+
+  private
+
+  def status
+    200
+  end
+
+  def headers
+    { 'Content-Type' => 'text/plain' }
+  end
+
+  def body
+    ["Hello, World!\n"]
+  end
+end
