57a75237f582ca6e69ec3a9cb3114844a7e4c4f7
konoplev-roman <roman@konoplev.pro>
2020-01-25 20:27:44 +0300

    Add parsing the time format

--- app.rb
+++ app.rb
@@ -12,11 +12,23 @@ class App
   def perform_request(request)
     return not_found unless request.get? && request.path == '/time'
 
-    build_response("Hello, World!\n")
+    time_format = parse_time_format(request)
+
+    return bad_request('Missing time format') if time_format.empty?
+
+    build_response(time_format.inspect)
+  end
+
+  def parse_time_format(request)
+    request.params['format'].to_s.strip
   end
 
   def not_found
     build_response("Not found!\n", status: 404)
+  end
+
+  def bad_request(msg)
+    build_response("#{msg}\n", status: 400)
   end
 
   def build_response(body, status: 200)
